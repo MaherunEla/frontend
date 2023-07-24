c0ea6eadfa4471daf7f8a62c63588fe50fa05e3f
MaherunEla <meherunela2002@gmail.com>
2023-07-24 17:31:32 +0600

    29

--- src/app/Components/view/blog/AllBlogdata.js
+++ src/app/Components/view/blog/AllBlogdata.js
@@ -264,6 +264,31 @@ export const AllBlogdata = [
         subTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         authors: {
+          image: "/assets/images/Home/author2.png",
+          title: "Emily Watson",
+          slug: "emily-watson",
+          address: "San Francisco, USA",
+          subTitle: "AI Researcher @TechCo",
+          bio: {
+            title: "AI Researcher",
+            description: "Emily Watson is a passionate AI researcher, working on cutting-edge technologies that shape the future of artificial intelligence. With a background in computer science and machine learning, she aims to make AI more accessible and beneficial for society."
+          }
+        }
+      },
+      {
+        id: 12,
+        image: "/assets/images/Blog/post3.png",
+        category: {
+          name: "Technology",
+          slug: "technology"
+        },
+        slug: "the-future-of-artificial-intelligence-and-its-impact-on-society",
+        icon: "/assests/images/home/catagory2.svg",
+        date: "2023-07-15",
+        title: "The Future of Artificial Intelligence and Its Impact on Society",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        authors: {
           image: "/assests/images/home/author2.png",
           title: "Emily Watson",
           slug: "emily-watson",
@@ -274,7 +299,208 @@ export const AllBlogdata = [
             description: "Emily Watson is a passionate AI researcher, working on cutting-edge technologies that shape the future of artificial intelligence. With a background in computer science and machine learning, she aims to make AI more accessible and beneficial for society."
           }
         }
-      }
+      },
+      {
+        id: 13,
+        image: "/assets/images/Blog/post3.png",
+        category: {
+          name: "Technology",
+          slug: "technology"
+        },
+        slug: "the-future-of-artificial-intelligence-and-its-impact-on-society",
+        icon: "/assests/images/home/catagory2.svg",
+        date: "2023-07-15",
+        title: "The Future of Artificial Intelligence and Its Impact on Society",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        authors: {
+          image: "/assests/images/home/author2.png",
+          title: "Emily Watson",
+          slug: "emily-watson",
+          address: "San Francisco, USA",
+          subTitle: "AI Researcher @TechCo",
+          bio: {
+            title: "AI Researcher",
+            description: "Emily Watson is a passionate AI researcher, working on cutting-edge technologies that shape the future of artificial intelligence. With a background in computer science and machine learning, she aims to make AI more accessible and beneficial for society."
+          }
+        }
+      },
+      {
+        id: 14,
+        image: "/assets/images/Blog/post3.png",
+        category: {
+          name: "Technology",
+          slug: "technology"
+        },
+        slug: "the-future-of-artificial-intelligence-and-its-impact-on-society",
+        icon: "/assests/images/home/catagory2.svg",
+        date: "2023-07-15",
+        title: "The Future of Artificial Intelligence and Its Impact on Society",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        authors: {
+          image: "/assests/images/home/author2.png",
+          title: "Emily Watson",
+          slug: "emily-watson",
+          address: "San Francisco, USA",
+          subTitle: "AI Researcher @TechCo",
+          bio: {
+            title: "AI Researcher",
+            description: "Emily Watson is a passionate AI researcher, working on cutting-edge technologies that shape the future of artificial intelligence. With a background in computer science and machine learning, she aims to make AI more accessible and beneficial for society."
+          }
+        }
+      },
+      {
+        id: 15,
+        image: "/assets/images/Blog/post3.png",
+        category: {
+          name: "Technology",
+          slug: "technology"
+        },
+        slug: "the-future-of-artificial-intelligence-and-its-impact-on-society",
+        icon: "/assests/images/home/catagory2.svg",
+        date: "2023-07-15",
+        title: "The Future of Artificial Intelligence and Its Impact on Society",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        authors: {
+          image: "/assests/images/home/author2.png",
+          title: "Emily Watson",
+          slug: "emily-watson",
+          address: "San Francisco, USA",
+          subTitle: "AI Researcher @TechCo",
+          bio: {
+            title: "AI Researcher",
+            description: "Emily Watson is a passionate AI researcher, working on cutting-edge technologies that shape the future of artificial intelligence. With a background in computer science and machine learning, she aims to make AI more accessible and beneficial for society."
+          }
+        }
+      },
+      {
+        id: 16,
+        image: "/assets/images/Blog/post3.png",
+        category: {
+          name: "Technology",
+          slug: "technology"
+        },
+        slug: "the-future-of-artificial-intelligence-and-its-impact-on-society",
+        icon: "/assests/images/home/catagory2.svg",
+        date: "2023-07-15",
+        title: "The Future of Artificial Intelligence and Its Impact on Society",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        authors: {
+          image: "/assests/images/home/author2.png",
+          title: "Emily Watson",
+          slug: "emily-watson",
+          address: "San Francisco, USA",
+          subTitle: "AI Researcher @TechCo",
+          bio: {
+            title: "AI Researcher",
+            description: "Emily Watson is a passionate AI researcher, working on cutting-edge technologies that shape the future of artificial intelligence. With a background in computer science and machine learning, she aims to make AI more accessible and beneficial for society."
+          }
+        }
+      },
+      {
+        id: 17,
+        image: "/assets/images/Blog/post2.png",
+        category: {
+          name:"Business",
+          slug:"business"
+        },
+        slug:" ad",
+        date: "2023-07-17",
+        icon: '/assets/images/Home/catagory1.svg',
+        title: "Fashion Trends: Latest Styles and Must-Haves",
+        subTitle: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+        content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+        authors: {
+          image: '/assets/images/Home/author2.png',
+          title: "Robert Fox",
+          slug:"robert-fox",
+          address: "New york, USA",
+          subTitle: "Content Writer @Company",
+          bio: {
+            title: "Author and Designer",
+            description: "John Doe is an experienced author and designer, specializing in user interface design and user experience. With a keen eye for detail and a passion for creating visually appealing and intuitive designs, he has helped numerous startups establish their brand presence."
+          }
+        }
+      },
+      {
+        id: 18,
+        image: "/assets/images/Blog/post3.png",
+        category: {
+          name:"Business",
+          slug:"business"
+        },
+        slug:" ad",
+      date: "2023-07-19",
+      icon: '/assets/images/home/catagory1.svg',
+      title: "Fashion Trends: Latest Styles and Must-Haves",
+      subTitle: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+      content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+        authors: {
+          image: '/assests/images/home/author4.png',
+          title: "Floyd Miles",
+          slug:"floyd-miles",
+          address: "New york, USA",
+          subTitle: "Content Writer @Company",
+          bio: {
+            title: "Author and Designer",
+            description: "John Doe is an experienced author and designer, specializing in user interface design and user experience. With a keen eye for detail and a passion for creating visually appealing and intuitive designs, he has helped numerous startups establish their brand presence."
+          }
+        }
+      },
+      {
+        id: 19,
+        image: "/assets/images/Blog/post4.png",
+        category: {
+          name:"Business",
+          slug:"business"
+        },
+        slug:" ad",
+      date: "2023-07-19",
+      icon: '/assets/images/home/catagory3.svg',
+      title: "Fashion Trends: Latest Styles and Must-Haves",
+      subTitle: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+      content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+        authors: {
+          image: '/assests/images/home/author4.png',
+          title: "Floyd Miles",
+          slug:"floyd-miles",
+          address: "New york, USA",
+          subTitle: "Content Writer @Company",
+          bio: {
+            title: "Author and Designer",
+            description: "John Doe is an experienced author and designer, specializing in user interface design and user experience. With a keen eye for detail and a passion for creating visually appealing and intuitive designs, he has helped numerous startups establish their brand presence."
+          }
+        }
+      },
+  
+      {
+          id: 20,
+          image: "/assets/images/Blog/post5.png",
+          category: {
+            name: "Technology",
+            slug: "technology"
+          },
+          slug: "the-future-of-artificial-intelligence-and-its-impact-on-society",
+          icon: "/assests/images/home/catagory2.svg",
+          date: "2023-07-15",
+          title: "The Future of Artificial Intelligence and Its Impact on Society",
+          subTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          authors: {
+            image: "/assests/images/home/author2.png",
+            title: "Emily Watson",
+            slug: "emily-watson",
+            address: "San Francisco, USA",
+            subTitle: "AI Researcher @TechCo",
+            bio: {
+              title: "AI Researcher",
+              description: "Emily Watson is a passionate AI researcher, working on cutting-edge technologies that shape the future of artificial intelligence. With a background in computer science and machine learning, she aims to make AI more accessible and beneficial for society."
+            }
+          }
+        },
 
 
 
